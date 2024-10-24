b5195d8452ce0e2a97878d9d3e5ec8a4780fb91e
KauanHenrique16 <106541032+KauanHenrique16@users.noreply.github.com>
2024-10-24 13:43:38 -0300

    ghfghgfhfg

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -4,8 +4,8 @@ async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-    const nomeDasRedes = Object.keys(dados)
-    const quantidadeDeUsuarios = Object.values(dados)
+    const nomeDasRedes = ['Estados Unidos','China','Japão','Alemanha','França','Brasil','Reino Unido','Itália','Rússia','India']
+    const quantidadeDeUsuarios = ['15.495.389.000','7.744.133.000','6.152.642.000','3.707.790.000','2.888.907.000','2.616.986.000','2.603.880.000','2.287.703.000','2.117.245.000','2.012.760.000']
 
     const data = [
         {
