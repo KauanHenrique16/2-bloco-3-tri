940a093526d493073a5625b420e1712e3dff0527
KauanHenrique16 <106541032+KauanHenrique16@users.noreply.github.com>
2024-11-07 15:22:39 -0300

    b b b

--- graficos/gr-3.js
+++ graficos/gr-3.js
@@ -4,8 +4,8 @@ async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-    const nomeDasRedes = ['Liam Lawson','']
-    const quantidadeDeUsuarios = ['']
+    const nomeDasRedes = ['Liam Lawson','Franco Colapinto','Oliver Bearman','Daniel Ricciardo','Alexander Albon','Kevin Magnussen','Esteban Ocon','Lance Stroll','Pierre Gasly','Yuki Tsunoda','Nico Hulkenberg','Fernando Alonso','Sergio Perez','Lewis Hamilton','George Russell','Carlos Sainz Jnr','Oscar piastri','Chales Leclerc','Lando Norris','Max Verstappem']
+    const quantidadeDeUsuarios = ['4','5','7','12','12','14','23','24','26','28','31','62','151','190','192','244','262','307','331','393']
 
     const data = [
         {
