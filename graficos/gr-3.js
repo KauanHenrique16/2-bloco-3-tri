55d6db11ec7c4b555369e88a839a3a3ceac5a226
KauanHenrique16 <106541032+KauanHenrique16@users.noreply.github.com>
2024-11-07 13:34:35 -0300

    4444

---
+++ graficos/gr-3.js
@@ -0,0 +1,57 @@
+import { getCSS, tickConfig, criarGrafico } from "./common.js"
+
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = ['Estados Unidos','China','Japão','Alemanha','França','Brasil','Reino Unido','Itália','Rússia','India']
+    const quantidadeDeUsuarios = ['15495389000','7744133000','6152642000','3707790000','2888907000','2616986000','2603880000','2287703000','2117245000','2012760000']
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'line',
+            marker: {
+                color: getCSS('--primary-color')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Os 10 países mais ricos do mundo',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'países',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'valor de dólares ',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        }
+
+    }
+
+    criarGrafico(data, layout)
+}
+
+quantidadeUsuariosPorRede()
