e976efab351ae2450b1b15f4f112c55db2c53195
KauanHenrique16 <106541032+KauanHenrique16@users.noreply.github.com>
2024-11-14 15:21:17 -0300

    uvcgcfy

--- graficos/gr-4.js
+++ graficos/gr-4.js
@@ -4,7 +4,7 @@ async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-    const nomeDasRedes = ['']
+    const nomeDasRedes = ['2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021']
     const quantidadeDeUsuarios = ['']
 
     const data = [
